6f3f1002e415cedfd9c273be1d9a3d446325c6fc
Zohra Ali <zohraali@Zohras-MBP.fios-router.home>
2019-11-27 23:43:01 -0500

    fixing merge issue

--- heal-city/app/src/main/java/cmsc436/rpg/healcity/ui/tutorial/IntroFragment1.kt
+++ heal-city/app/src/main/java/cmsc436/rpg/healcity/ui/tutorial/IntroFragment1.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import cmsc436.rpg.healcity.R
+import kotlinx.android.synthetic.*
+//import android.R
+
+
 
 
 class IntroFragment1 : Fragment() {
@@ -15,6 +20,13 @@ class IntroFragment1 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        val f = childFragmentManager
+        val pressed = f.findFragmentById(R.id.btn)
+
+        //val pressed = getView().findViewById(R.id.btn)
+        // val pressed = getView().findFragmentByTag(R.id.btn)
+
+
         return inflater.inflate(R.layout.fragment_intro_fragment1, container, false)
     }
 
